5df6be40257dfaf07f92ecdf66f0a2bff4a42345
Madhura <madhura.bhujbal@gmail.com>
2020-08-05 12:59:38 +0100

    Add react weather app

---
+++ weather/precache-manifest.1a66b126378c18093b339bc12c570a2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c1e002b6e7b3af9a10538231525499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4935e38104e91a48b20b",
+    "url": "/static/css/main.c064d954.chunk.css"
+  },
+  {
+    "revision": "3880c5915ebbc53a74a7",
+    "url": "/static/js/2.264a79ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.264a79ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4935e38104e91a48b20b",
+    "url": "/static/js/main.e89a5787.chunk.js"
+  },
+  {
+    "revision": "769a5aa6c6ba2419dda2",
+    "url": "/static/js/runtime-main.3368d19e.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
